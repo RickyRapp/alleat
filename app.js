7b6016cffa19b67d8570d9000cfb239f93d2a309
Ricky <rickyrappaport10@gmail.com>
2022-05-25 10:44:11 -0500

    changed app.js

--- app.js
+++ app.js
@@ -3,20 +3,20 @@ const app = express();
 const mongoose = require("mongoose");
 const bodyParser = require('body-parser');
 //const Router = express.Router();
-require('dotenv/config') 
+require('dotenv/config')
 
 app.use(bodyParser.json());
 
 //midleware
 
 const categoryRoute = require('./routes/categories')
-app.use('/categories', categoryRoute )
+app.use('/categories', categoryRoute)
 
 const restaurantRoute = require('./routes/restaurants')
-app.use('/restaurants', restaurantRoute )
+app.use('/restaurants', restaurantRoute)
 
 const bookingRoute = require('./routes/bookings')
-app.use('/bookings', bookingRoute )
+app.use('/bookings', bookingRoute)
 
 
 
@@ -25,21 +25,21 @@ app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
 });
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
   res.send('we on home')
 })
- 
 
 
-  //connect to db
-  mongoose.connect(
-    process.env.DB_CONNECTION, 
-  {useNewUrlParser: true},
+
+//connect to db
+mongoose.connect(
+  process.env.DB_CONNECTION,
+  { useNewUrlParser: true },
   () => {
-      console.log('connected to db')
+    console.log('connected to db')
   })
-      
-    
+
+
 app.listen(3001, () => {
-   console.log("Server is running at port 3001");
+  console.log("Server is running at port 3001");
 });
